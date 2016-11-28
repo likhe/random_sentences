095a39df79b39911983b147e5d07e144181b2a1e
likhe <likhelifi@laposte.net>
2016-11-28 16:35:07 +0100

    write correction

---
+++ app.js
@@ -0,0 +1,11 @@
+const subjects = ["Tito","Tata","Toto","Tati"];
+const verbe = ["marche","mange", "dort", "parle"];
+const complements = ["beaucoup", "vite", "encore", "correctement"];
+
+
+let genSentences = function(subjects, verbe, complements) {
+  var sIndex = ;
+  var vIndex = ;
+  var cIndex = ;
+  return subjects + " " + verbe + " " + complements;
+}
